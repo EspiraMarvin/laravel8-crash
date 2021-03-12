110e5d97530484a791dacf74158f1353971376aa
Marvin Espira <espiramarvin@gmail.com>
2021-03-12 13:54:54 +0300

    fixed the remember me request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,10 +27,9 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        if (!auth()->attempt($request->only('email','password', $request->remember))){
+        if (!auth()->attempt($request->only('email','password'),$request->remember)){
             return back()->with('status', 'Invalid Login Credentials');
         }
-
         return redirect()->route('dashboard');
     }
 }
